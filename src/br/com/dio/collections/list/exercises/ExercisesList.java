c096365871952995f10c82c43eb2f10e9d870cea
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-04-15 14:42:24 +0100

    methods list - part 2

---
+++ src/br/com/dio/collections/list/exercises/ExercisesList.java
@@ -0,0 +1,13 @@
+package br.com.dio.collections.list.exercises;
+
+public class ExercisesList {
+    public static void main(String[] args) {
+        // Resolva esses exercicios utilizando os metodos da implementaão LinkedList
+
+        System.out.println("Crie um alista chamada notas2 e coloque todos os elementos da list Arraylist" +
+                "nessa nova lista: ");
+
+        System.out.println("Mostre a primeira nota da nova lista sem removê-lo: ");
+        System.out.println("Mostre a primeira nota da nova lista removendo-o: ");
+    }
+}
